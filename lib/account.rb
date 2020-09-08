99494c9ce0a0773ddefc7a27e318fb4dfe287db2
Matteo Micozzi <micozzimatteo@yahoo.it>
2020-09-08 19:56:58 +0100

    beginning of translation process from js to ruby

---
+++ lib/account.rb
@@ -0,0 +1,7 @@
+# ####
+# class Account {
+#   constructor(number_of_account) {
+#     this.ACCOUNT_NUMBER = number_of_account;
+#   };
+# };
+# ####
--- lib/bank.rb
+++ lib/bank.rb
@@ -1,2 +1,17 @@
 require 'require_all'
 require_all 'lib'
+
+#####
+# class Bank {
+#   constructor() {
+#     this.accounts = [];
+#   };
+#   createAccount(number_account) {
+#     this.accounts.push(new Account(number_account));
+#   };
+#    all() {
+#      let list = this.accounts.map(num => num.ACCOUNT_NUMBER);
+#      list;
+#    };
+# };
+######
