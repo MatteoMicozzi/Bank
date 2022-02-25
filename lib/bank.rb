2a0f68855041a2d1af03f1aec44f23023c60b461
MatteoMicozzi <micozzimatteo@yahoo.it>
2022-02-25 23:33:28 +0000

    Testing Bank making new bank and checking #bank_number.

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,15 +2,12 @@ require 'require_all'
 require_all 'lib'
 
 class Bank
+  attr_reader :bank_number
   def initialize(bank_number)
     @bank_number = "#{bank_number}"
     @accounts = {}
   end
 
-  def bank_number
-    @bank_number
-  end
-  
   def create_account(account_number, account = Account.new)
     @accounts[:"#{account_number}"] = account
   end
