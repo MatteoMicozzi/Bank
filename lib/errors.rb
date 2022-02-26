d0ecca2180ffd9675e775347a13268ce978b1afe
MatteoMicozzi <micozzimatteo@yahoo.it>
2022-02-26 06:44:24 +0000

    Fixed indentation errors.rb.

--- lib/errors.rb
+++ lib/errors.rb
@@ -9,20 +9,20 @@ class WithdrawalError < StandardError; end
 class DepositError < StandardError; end
 
 
-  def accounts_availability(accounts)
-    raise NoAccountError, "No account available!" unless accounts.size > 0
-  end
-  def validation_number(number)
-    raise AccountNumberError, "Invalid account number! Please enter a 'number' with quotations." unless number.is_a? String
-  end
-  def availability_account(accounts, account_number)
-    raise UnavailableAccountError, "No account available!" unless accounts.include?(account_number)
-  end
+def accounts_availability(accounts)
+  raise NoAccountError, "No account available!" unless accounts.size > 0
+end
+def validation_number(number)
+  raise AccountNumberError, "Invalid account number! Please enter a 'number' with quotations." unless number.is_a? String
+end
+def availability_account(accounts, account_number)
+  raise UnavailableAccountError, "No account available!" unless accounts.include?(account_number)
+end
 
 
-  def money_availability(balance, amount)
-    raise WithdrawalError, "Invalid amount! Valid amount between 0 and #{balance}." unless ((balance - amount) > 0) && amount > 0
-  end
-  def validation_deposit(amount)
-    raise DepositError, "Invalid amount! Valid amount above 0." unless amount > 0
-  end
+def money_availability(balance, amount)
+  raise WithdrawalError, "Invalid amount! Valid amount between 0 and #{balance}." unless ((balance - amount) > 0) && amount > 0
+end
+def validation_deposit(amount)
+  raise DepositError, "Invalid amount! Valid amount above 0." unless amount > 0
+end
