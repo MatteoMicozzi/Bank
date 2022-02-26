2e3a4da3731b22ef9a97e44279d18e5b28885f6f
MatteoMicozzi <micozzimatteo@yahoo.it>
2022-02-26 00:40:52 +0000

    Final Tests Bank Class.

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,13 +2,28 @@ require 'require_all'
 require_all 'lib'
 
 class Bank
-  attr_reader :bank_number
+  attr_reader :bank_number, :accounts
+
   def initialize(bank_number)
     @bank_number = "#{bank_number}"
     @accounts = {}
   end
 
+  def all
+    list = []
+    @accounts.each { |number, object| list.push(number) }
+    list.join('\n')
+  end
+
   def create_account(account_number, account = Account.new)
-    @accounts[:"#{account_number}"] = account
+    @accounts[account_number] = account
+  end
+
+  def select_account(account_number)
+    @accounts[account_number]
+  end
+
+  def delete_account(account_number)
+    @accounts.delete(account_number)
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,14 +5,45 @@ describe Bank do
   describe '#bank_number' do
     it "will print the bank number" do
       bank01 = Bank.new('01')
-      bank10 = Bank.new(10)
       expect(bank01.bank_number).to eq('01')
-      expect(bank10.bank_number).to eq('10')
     end
   end
-  # describe '#' do
-  #   it "" do
-  #     expect().to eq()
-  #   end
-  # end
+
+  describe '#create_account' do
+    it "will create a new account" do
+      bank01 = Bank.new('01')
+      bank01.create_account('01')
+      expect(bank01.accounts.size).to eq(1)
+    end
+  end
+
+  describe '#all' do
+    it "will bring back all the accounts numbers" do
+      bank01 = Bank.new('01')
+      bank01.create_account('01')
+      bank01.create_account('02')
+      bank01.create_account('03')
+      expect(bank01.all).to eq('01\n02\n03')
+    end
+  end
+
+  describe '#select_account' do
+    it "will select an existing account" do
+      bank01 = Bank.new('01')
+      bank01.create_account('01')
+      expect(bank01.select_account('01').is_a? Account).to eq(true)
+    end
+  end
+
+  describe '#delete_account' do
+    it "will delete an existing account" do
+      bank01 = Bank.new('01')
+      bank01.create_account('01')
+      bank01.create_account('02')
+      bank01.create_account('03')
+      expect(bank01.all).to eq('01\n02\n03')
+      bank01.delete_account('01')
+      expect(bank01.all).to eq('02\n03')
+    end
+  end
 end
